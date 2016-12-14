8952bfa88ff4b266559ba5f695357b65b5e7ea76
aisatriani <aisatriani03@gmail.com>
2016-12-14 13:01:14 +0800

    add seeder class data lokasi dosen

---
+++ database/seeds/LokasiDosenTableSeeder.php
@@ -0,0 +1,523 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LokasiDosenTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('lokasi_dosen')->delete();
+        
+        \DB::table('lokasi_dosen')->insert(array (
+            0 => 
+            array (
+
+                'nidn' => '0003097303',
+                'latitude' => 0.57221,
+                'longitude' => 123.06007,
+                'created_at' => '2016-12-11 01:29:09',
+                'updated_at' => '2016-12-11 01:29:09',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            1 => 
+            array (
+
+                'nidn' => '0007046907',
+                'latitude' => 0.56105000000000005,
+                'longitude' => 123.0421,
+                'created_at' => '2016-12-11 01:32:12',
+                'updated_at' => '2016-12-11 01:32:12',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            2 => 
+            array (
+
+                'nidn' => '0003047410',
+                'latitude' => 0.56713000000000002,
+                'longitude' => 123.04537999999999,
+                'created_at' => '2016-12-11 01:35:16',
+                'updated_at' => '2016-12-11 01:35:16',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            3 => 
+            array (
+
+                'nidn' => '0004017703',
+                'latitude' => 0.56405000000000005,
+                'longitude' => 123.03591,
+                'created_at' => '2016-12-11 01:37:26',
+                'updated_at' => '2016-12-11 01:37:26',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            4 => 
+            array (
+
+                'nidn' => '0021117702',
+                'latitude' => 0.56093999999999999,
+                'longitude' => 123.07517,
+                'created_at' => '2016-12-11 01:41:44',
+                'updated_at' => '2016-12-11 01:41:44',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            5 => 
+            array (
+
+                'nidn' => '0022127803',
+                'latitude' => 0.56113000000000002,
+                'longitude' => 123.07473,
+                'created_at' => '2016-12-11 01:42:33',
+                'updated_at' => '2016-12-11 01:42:33',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            6 => 
+            array (
+
+                'nidn' => '0023096502',
+                'latitude' => 0.56103999999999998,
+                'longitude' => 123.07494,
+                'created_at' => '2016-12-11 01:45:03',
+                'updated_at' => '2016-12-11 01:45:03',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            7 => 
+            array (
+
+                'nidn' => '0030047202',
+                'latitude' => 0.57925000000000004,
+                'longitude' => 123.0425,
+                'created_at' => '2016-12-11 01:47:06',
+                'updated_at' => '2016-12-11 01:47:06',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            8 => 
+            array (
+
+                'nidn' => '0030047603',
+                'latitude' => 0.56860999999999995,
+                'longitude' => 123.05358,
+                'created_at' => '2016-12-11 01:48:18',
+                'updated_at' => '2016-12-11 01:48:18',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            9 => 
+            array (
+
+                'nidn' => '0030116505',
+                'latitude' => 0.56501000000000001,
+                'longitude' => 123.04326,
+                'created_at' => '2016-12-11 01:49:49',
+                'updated_at' => '2016-12-11 01:49:49',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            10 => 
+            array (
+
+                'nidn' => '0004017403',
+                'latitude' => 0.59899999999999998,
+                'longitude' => 123.07826,
+                'created_at' => '2016-12-11 01:53:15',
+                'updated_at' => '2016-12-11 01:53:15',
+                'kabkot' => 'BONE BOLANGO',
+            ),
+            11 => 
+            array (
+
+                'nidn' => '0017107504',
+                'latitude' => 0.54932000000000003,
+                'longitude' => 123.05767,
+                'created_at' => '2016-12-11 01:55:13',
+                'updated_at' => '2016-12-11 01:55:13',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            12 => 
+            array (
+
+                'nidn' => '0024067502',
+                'latitude' => 0.56372,
+                'longitude' => 123.04201,
+                'created_at' => '2016-12-11 01:56:48',
+                'updated_at' => '2016-12-11 01:56:48',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            13 => 
+            array (
+
+                'nidn' => '0027017803',
+                'latitude' => 0.54820999999999998,
+                'longitude' => 123.05493,
+                'created_at' => '2016-12-11 01:57:42',
+                'updated_at' => '2016-12-11 01:57:42',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            14 => 
+            array (
+
+                'nidn' => '0027047604',
+                'latitude' => 0.54481000000000002,
+                'longitude' => 123.06279000000001,
+                'created_at' => '2016-12-11 01:58:56',
+                'updated_at' => '2016-12-11 01:58:56',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            15 => 
+            array (
+
+                'nidn' => '0028017204',
+                'latitude' => 0.57923000000000002,
+                'longitude' => 123.0425,
+                'created_at' => '2016-12-11 02:00:57',
+                'updated_at' => '2016-12-11 02:00:57',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            16 => 
+            array (
+
+                'nidn' => '0003107401',
+                'latitude' => 0.55169000000000001,
+                'longitude' => 123.11623,
+                'created_at' => '2016-12-11 02:03:09',
+                'updated_at' => '2016-12-11 02:03:09',
+                'kabkot' => 'BONE BOLANGO',
+            ),
+            17 => 
+            array (
+
+                'nidn' => '0005047007',
+                'latitude' => 0.55879999999999996,
+                'longitude' => 123.03439,
+                'created_at' => '2016-12-11 02:05:13',
+                'updated_at' => '2016-12-11 02:05:13',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            18 => 
+            array (
+
+                'nidn' => '0005076805',
+                'latitude' => 0.56550999999999996,
+                'longitude' => 123.06413999999999,
+                'created_at' => '2016-12-11 02:06:10',
+                'updated_at' => '2016-12-11 02:06:10',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            19 => 
+            array (
+
+                'nidn' => '0016017405',
+                'latitude' => 0.55698999999999999,
+                'longitude' => 123.04563,
+                'created_at' => '2016-12-11 02:07:10',
+                'updated_at' => '2016-12-11 02:07:10',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            20 => 
+            array (
+
+                'nidn' => '0021046705',
+                'latitude' => 0.56200000000000006,
+                'longitude' => 123.04806000000001,
+                'created_at' => '2016-12-11 02:08:03',
+                'updated_at' => '2016-12-11 02:08:03',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            21 => 
+            array (
+
+                'nidn' => '0022027105',
+                'latitude' => 0.58321000000000001,
+                'longitude' => 123.03585,
+                'created_at' => '2016-12-11 02:08:56',
+                'updated_at' => '2016-12-11 02:08:56',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            22 => 
+            array (
+                'nidn' => '0025117408',
+                'latitude' => 0.62605999999999995,
+                'longitude' => 122.98475999999999,
+                'created_at' => '2016-12-11 02:09:55',
+                'updated_at' => '2016-12-11 02:09:55',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            23 => 
+            array (
+                'nidn' => '0008127805',
+                'latitude' => 0.57662999999999998,
+                'longitude' => 123.04118,
+                'created_at' => '2016-12-11 03:57:58',
+                'updated_at' => '2016-12-11 03:57:58',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            24 => 
+            array (
+                'nidn' => '0010127701',
+                'latitude' => 0.52944000000000002,
+                'longitude' => 123.06082000000001,
+                'created_at' => '2016-12-11 04:09:35',
+                'updated_at' => '2016-12-11 04:09:35',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            25 => 
+            array (
+                'nidn' => '0014058301',
+                'latitude' => 0.53378000000000003,
+                'longitude' => 123.05504000000001,
+                'created_at' => '2016-12-11 04:10:15',
+                'updated_at' => '2016-12-11 04:10:15',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            26 => 
+            array (
+                'nidn' => '0014108001',
+                'latitude' => 0.55242000000000002,
+                'longitude' => 123.04962999999999,
+                'created_at' => '2016-12-11 04:10:51',
+                'updated_at' => '2016-12-11 04:10:51',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            27 => 
+            array (
+                'nidn' => '0015057903',
+                'latitude' => 0.57208000000000003,
+                'longitude' => 123.04807,
+                'created_at' => '2016-12-11 04:11:24',
+                'updated_at' => '2016-12-11 04:11:24',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            28 => 
+            array (
+                'nidn' => '0023037603',
+                'latitude' => 0.56100000000000005,
+                'longitude' => 123.07459,
+                'created_at' => '2016-12-11 04:12:23',
+                'updated_at' => '2016-12-11 04:12:23',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            29 => 
+            array (
+                'nidn' => '0023108103',
+                'latitude' => 0.56262000000000001,
+                'longitude' => 123.09845,
+                'created_at' => '2016-12-11 04:13:39',
+                'updated_at' => '2016-12-11 04:13:39',
+                'kabkot' => 'BONE BOLANGO',
+            ),
+            30 => 
+            array (
+                'nidn' => '0024117503',
+                'latitude' => 0.56103999999999998,
+                'longitude' => 123.07453,
+                'created_at' => '2016-12-11 04:14:22',
+                'updated_at' => '2016-12-11 04:14:22',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            31 => 
+            array (
+                'nidn' => '0028057802',
+                'latitude' => 0.60592000000000001,
+                'longitude' => 123.0325,
+                'created_at' => '2016-12-11 04:15:30',
+                'updated_at' => '2016-12-11 04:15:30',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            32 => 
+            array (
+                'nidn' => '0030018301',
+                'latitude' => 0.57045000000000001,
+                'longitude' => 123.05443,
+                'created_at' => '2016-12-11 04:16:18',
+                'updated_at' => '2016-12-11 04:16:18',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            33 => 
+            array (
+                'nidn' => '0031037903',
+                'latitude' => 0.58301999999999998,
+                'longitude' => 123.03292999999999,
+                'created_at' => '2016-12-11 04:20:11',
+                'updated_at' => '2016-12-11 04:20:11',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            34 => 
+            array (
+                'nidn' => '0031108103',
+                'latitude' => 0.54979999999999996,
+                'longitude' => 123.05534,
+                'created_at' => '2016-12-11 04:21:34',
+                'updated_at' => '2016-12-11 04:21:34',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            35 => 
+            array (
+                'nidn' => '0002017206',
+                'latitude' => 0.54668000000000005,
+                'longitude' => 123.06549,
+                'created_at' => '2016-12-11 04:22:27',
+                'updated_at' => '2016-12-11 04:22:27',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            36 => 
+            array (
+                'nidn' => '0014028602',
+                'latitude' => 0.52842,
+                'longitude' => 123.0637,
+                'created_at' => '2016-12-11 04:23:00',
+                'updated_at' => '2016-12-11 04:23:00',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            37 => 
+            array (
+                'nidn' => '0016047303',
+                'latitude' => 0.56142999999999998,
+                'longitude' => 123.05043999999999,
+                'created_at' => '2016-12-11 04:23:29',
+                'updated_at' => '2016-12-11 04:23:29',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            38 => 
+            array (
+                'nidn' => '0017048001',
+                'latitude' => 0.62575999999999998,
+                'longitude' => 123.9933,
+                'created_at' => '2016-12-11 04:23:57',
+                'updated_at' => '2016-12-11 04:23:57',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            39 => 
+            array (
+                'nidn' => '0017087402',
+                'latitude' => 0.55205000000000004,
+                'longitude' => 123.04611,
+                'created_at' => '2016-12-11 04:24:24',
+                'updated_at' => '2016-12-11 04:24:24',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            40 => 
+            array (
+                'nidn' => '0020078303',
+                'latitude' => 0.53232999999999997,
+                'longitude' => 123.06088,
+                'created_at' => '2016-12-11 04:25:11',
+                'updated_at' => '2016-12-11 04:25:11',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            41 => 
+            array (
+                'nidn' => '0025078202',
+                'latitude' => 0.55173000000000005,
+                'longitude' => 123.06619000000001,
+                'created_at' => '2016-12-11 04:25:45',
+                'updated_at' => '2016-12-11 04:25:45',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            42 => 
+            array (
+                'nidn' => '0009126209',
+                'latitude' => 0.62461999999999995,
+                'longitude' => 122.98403,
+                'created_at' => '2016-12-11 04:26:40',
+                'updated_at' => '2016-12-11 04:26:40',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            43 => 
+            array (
+                'nidn' => '0011097406',
+                'latitude' => 0.54857999999999996,
+                'longitude' => 123.05122,
+                'created_at' => '2016-12-11 04:27:13',
+                'updated_at' => '2016-12-11 04:27:13',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            44 => 
+            array (
+                'nidn' => '0005027807',
+                'latitude' => 0.58279999999999998,
+                'longitude' => 123.03393,
+                'created_at' => '2016-12-11 04:29:37',
+                'updated_at' => '2016-12-11 04:29:37',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            45 => 
+            array (
+                'nidn' => '0007017603',
+                'latitude' => 0.55718000000000001,
+                'longitude' => 123.05309,
+                'created_at' => '2016-12-11 04:32:22',
+                'updated_at' => '2016-12-11 04:32:22',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            46 => 
+            array (
+                'nidn' => '0010106803',
+                'latitude' => 0.55327000000000004,
+                'longitude' => 123.51439999999999,
+                'created_at' => '2016-12-11 04:32:57',
+                'updated_at' => '2016-12-11 04:32:57',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            47 => 
+            array (
+                'nidn' => '0012017106',
+                'latitude' => 0.58304,
+                'longitude' => 123.03529,
+                'created_at' => '2016-12-11 04:34:00',
+                'updated_at' => '2016-12-11 04:34:00',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            48 => 
+            array (
+                'nidn' => '0013127704',
+                'latitude' => 0.58279000000000003,
+                'longitude' => 123.03393,
+                'created_at' => '2016-12-11 04:35:09',
+                'updated_at' => '2016-12-11 04:35:09',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            49 => 
+            array (
+                'nidn' => '0016077306',
+                'latitude' => 0.58918000000000004,
+                'longitude' => 123.04539,
+                'created_at' => '2016-12-11 04:36:04',
+                'updated_at' => '2016-12-11 04:36:04',
+                'kabkot' => 'KAB. GORONTALO',
+            ),
+            50 => 
+            array (
+                'nidn' => '0019027402',
+                'latitude' => 0.55559999999999998,
+                'longitude' => 123.05988000000001,
+                'created_at' => '2016-12-11 04:36:38',
+                'updated_at' => '2016-12-11 04:36:38',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            51 => 
+            array (
+                'nidn' => '0013018301',
+                'latitude' => 0.56583000000000006,
+                'longitude' => 123.03573,
+                'created_at' => '2016-12-11 04:37:58',
+                'updated_at' => '2016-12-11 04:37:58',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+            52 => 
+            array (
+                'nidn' => '0017067804',
+                'latitude' => 0.57177999999999995,
+                'longitude' => 123.05579,
+                'created_at' => '2016-12-11 04:50:29',
+                'updated_at' => '2016-12-11 04:50:29',
+                'kabkot' => 'KOTA GORONTALO',
+            ),
+        ));
+        
+        
+    }
+}
